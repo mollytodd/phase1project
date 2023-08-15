bf5054916bbe5d7b89a7f3d53cb0246002ef2aa7
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-08-15 10:20:26 -0700

    Update index.js
    add commas, add size, add toggle button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const countriesURL = "https://restcountries.com/v3.1/all?name";
+
 let selectedCountry;
 function getCountries(url) {
   // takes URL as argument. uses fetch to get the countries + a variety of .then and catch. error thrown if response not ok
@@ -16,7 +17,7 @@ function getCountries(url) {
 
 getCountries(countriesURL).then((countries) => {
   //invokes get countries and sets up subsequent steps with data (adding submit form)
-
+  
   const searchForm = document.getElementById("search-form"); // finds and stores a referece to the html form element with id search form
   searchForm.addEventListener("submit", function (event) {
     // adds event listener to the search form. when someone submits a country, the below happens
@@ -54,8 +55,36 @@ getCountries(countriesURL).then((countries) => {
 
 function newCountryCard(selectedCountry) {
 
+  
+  const toggleButton = document.createElement("button"); // toggle to display coat of arms/size/could be anything**
+  toggleButton.className = "toggle";
+  toggleButton.textContent = "Coat of Arms";
 
-  const deleteButton = document.createElement("button")
+  const hiddenContainer = document.createElement("div");
+  hiddenContainer.style.display = "none";
+
+  const coatOfArms = document.createElement("p");
+  coatOfArms.innerHTML = `<br>A design often used to signify a family or institution. They can describe the identity of a country with different shapes, colors, and animals.`
+
+  const pic = document.createElement("img")
+  pic.className = "coat"
+  pic.src = selectedCountry.coatOfArms.png;
+
+  hiddenContainer.appendChild(pic);
+  hiddenContainer.appendChild(coatOfArms);
+
+  let visible = false;
+  toggleButton.addEventListener('mouseover', (e) => {
+    
+    visible = !visible;
+    hiddenContainer.style.display = visible ? "block" : "none";
+  })
+  
+ 
+
+
+
+  const deleteButton = document.createElement("button") //creates delete button to get rid of card container
   deleteButton.className = "delete"
   deleteButton.textContent = "Delete"
   deleteButton.addEventListener('click', (e) => {
@@ -65,22 +94,35 @@ function newCountryCard(selectedCountry) {
   const cardContainer = document.createElement("div");
   cardContainer.classList.add("card");
   const img = document.createElement("img");
+  img.className = "flag"
   img.src = selectedCountry.flags.png;
+
   const h2 = document.createElement("h2");
   h2.textContent = selectedCountry.name.common;
+
   const population = document.createElement("p");
-  population.textContent = `Population: ${selectedCountry.population}`
+  const populationNumber = selectedCountry.population;
+  const commaPopulation = populationNumber.toLocaleString();
+  population.textContent = `Population: ${commaPopulation}`
+
   const capital = document.createElement("p");
   capital.textContent = `Capital: ${selectedCountry.capital}`;
+
   const languages = document.createElement("p");
-  languages.textContent = `Languages: ${Object.values(selectedCountry.languages)[0]}`
+  languages.textContent = `Languages: ${Object.values(selectedCountry.languages)}`
+
   const currency = document.createElement("span")
   const firstCurrencyKey = Object.keys(selectedCountry.currencies)[0]
   const firstCurrency = selectedCountry.currencies[firstCurrencyKey]
   currency.innerHTML = `Currency: ${firstCurrency.name}<br><br>Symbol: ${firstCurrency.symbol}`
+
   const subRegion = document.createElement("p");
   subRegion.textContent = `Region: ${selectedCountry.subregion}`;
-  
+
+  const size = document.createElement("p");
+  const sizeNumber = selectedCountry.area;
+  const commaSize = sizeNumber.toLocaleString();
+  size.innerHTML = `Size: ${commaSize} km&sup2`;
 
   cardContainer.appendChild(img);
   cardContainer.appendChild(h2);
@@ -89,9 +131,12 @@ function newCountryCard(selectedCountry) {
   cardContainer.appendChild(languages);
   cardContainer.appendChild(currency);
   cardContainer.appendChild(subRegion);
+  cardContainer.appendChild(size);
   cardContainer.appendChild(deleteButton);
- 
- 
+  cardContainer.appendChild(toggleButton);
+  cardContainer.appendChild(hiddenContainer);
+  
+  
 
 
   return cardContainer;
@@ -99,6 +144,8 @@ function newCountryCard(selectedCountry) {
 
   
 }
+
+
 
 //  
 // }
